9b06ec01e0910a5b0f816b5c2454c7e1ed064f0a
裕登 鎌田 <150265@jc-21.jp>
2017-11-02 16:18:37 +0900

    初回(initial)コミット

--- BMI/BMI/MainPage.xaml.cs
+++ BMI/BMI/MainPage.xaml.cs
@@ -13,5 +13,74 @@ namespace BMI
 		{
 			InitializeComponent();
 		}
-	}
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //DisplayAlert("身長", Height.Text, "OK");
+            //DisplayAlert("体重", Weight.Text, "OK");
+            //this.hoge.Text = int.Parse("this.Weight") / (((int)this.Height / 100) ^ 2);
+
+            double h = double.Parse(Height.Text);
+            double w = double.Parse(Weight.Text);
+            if(h >= 3)
+            {
+                h /= 100;
+            }
+            double bmi = w / h / h;
+
+            //DisplayAlert("BMI", bmi.ToString(), "OK");
+
+            string x = bmi.ToString();
+            hoge.Text = x;
+
+            /*
+            if (bmi < 18.5)
+            {
+                DisplayAlert("BMI","BMI値は"+ bmi.ToString()+"でやせ型です", "OK");
+            }else if(bmi < 25){
+                DisplayAlert("BMI", "BMI値は" + bmi.ToString() + "で普通体重です", "OK");
+            }
+            else if (bmi < 30)
+            {
+                DisplayAlert("BMI", "BMI値は" + bmi.ToString() + "で肥満(1度)です", "OK");
+            }
+            else if (bmi < 35)
+            {
+                DisplayAlert("BMI", "BMI値は" + bmi.ToString() + "で肥満(2度)です", "OK");
+            }
+            else if (bmi < 40)
+            {
+                DisplayAlert("BMI", "BMI値は" + bmi.ToString() + "で肥満(3度)です", "OK");
+            }
+            else
+            {
+                DisplayAlert("BMI", "BMI値は" + bmi.ToString() + "で肥満(4度)です", "OK");
+            }
+            */
+            if (bmi < 18.5)
+            {
+                DisplayAlert("BMI", "BMI値は" + bmi.ToString() + "でやせ型です", "OK");
+            }
+            else if (bmi < 25)
+            {
+                DisplayAlert("BMI", "BMI値は" + bmi.ToString() + "で普通体重です", "OK");
+            }
+            else if (bmi < 30)
+            {
+                DisplayAlert("BMI", "BMI値は" + bmi.ToString() + "で肥満(1度)です", "OK");
+            }
+            else if (bmi < 35)
+            {
+                DisplayAlert("BMI", "BMI値は" + bmi.ToString() + "で肥満(2度)です", "OK");
+            }
+            else if (bmi < 40)
+            {
+                DisplayAlert("BMI", "BMI値は" + bmi.ToString() + "で肥満(3度)です", "OK");
+            }
+            else
+            {
+                DisplayAlert("BMI", "BMI値は" + bmi.ToString() + "で肥満(4度)です", "OK");
+            }
+
+        }
+    }
 }
